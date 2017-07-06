1f5998e10b343e8b08f717a39658161d07b941cd
guardiantrust <aaronpeterson3@gmail.com>
2017-07-06 16:43:23 -0600

    more changes

--- config/components/server.js
+++ config/components/server.js
@@ -20,3 +20,25 @@ const config = {
 }
 
 module.exports = config
+
+var Joi = require('joi');
+
+const schema = Joi.object().keys({
+    env: Joi.string().alphanum(),
+    log_level: Joi.string(),
+    cache_port: Joi.number().min(1000).max(99999)
+})
+
+const {err, value: envVars } = Joi.validate(process.env, schema)
+
+if(err) {
+    throw new Error('Error with config in EventStream');
+}
+
+const config = {
+    url: envVars.CACHE_URL,
+    port: envVars.CACHE_PORT
+}
+
+
+module.exports = config
